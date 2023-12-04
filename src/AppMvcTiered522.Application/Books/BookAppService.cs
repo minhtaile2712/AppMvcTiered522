e6a02e9f1ca0bc64b3749932b5b78ec6ac7a9b2e
minhtaile2712 <minhtaile2712@gmail.com>
2023-12-04 12:06:11 +0700

    Add Book authorization

--- src/AppMvcTiered522.Application/Books/BookAppService.cs
+++ src/AppMvcTiered522.Application/Books/BookAppService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppMvcTiered522.Permissions;
+using System;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
@@ -18,5 +19,10 @@ public class BookAppService :
         IRepository<Book, Guid> repository
     ) : base(repository)
     {
+        GetPolicyName = AppMvcTiered522Permissions.Books.Default;
+        GetListPolicyName = AppMvcTiered522Permissions.Books.Default;
+        CreatePolicyName = AppMvcTiered522Permissions.Books.Create;
+        UpdatePolicyName = AppMvcTiered522Permissions.Books.Edit;
+        DeletePolicyName = AppMvcTiered522Permissions.Books.Delete;
     }
 }
