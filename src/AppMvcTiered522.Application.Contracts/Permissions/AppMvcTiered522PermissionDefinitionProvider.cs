ee6b749766a44858a2f160744a1e6983902e2d6f
minhtaile2712 <minhtaile2712@gmail.com>
2023-12-04 12:01:26 +0700

    Add permissions

--- src/AppMvcTiered522.Application.Contracts/Permissions/AppMvcTiered522PermissionDefinitionProvider.cs
+++ src/AppMvcTiered522.Application.Contracts/Permissions/AppMvcTiered522PermissionDefinitionProvider.cs
@@ -11,6 +11,16 @@ public class AppMvcTiered522PermissionDefinitionProvider : PermissionDefinitionP
         var myGroup = context.AddGroup(AppMvcTiered522Permissions.GroupName);
         //Define your own permissions here. Example:
         //myGroup.AddPermission(AppMvcTiered522Permissions.MyPermission1, L("Permission:MyPermission1"));
+
+        var booksPermission = myGroup.AddPermission(AppMvcTiered522Permissions.Books.Default, L("Permission:Books"));
+        booksPermission.AddChild(AppMvcTiered522Permissions.Books.Create, L("Permission:Books.Create"));
+        booksPermission.AddChild(AppMvcTiered522Permissions.Books.Edit, L("Permission:Books.Edit"));
+        booksPermission.AddChild(AppMvcTiered522Permissions.Books.Delete, L("Permission:Books.Delete"));
+
+        var authorsPermission = myGroup.AddPermission(AppMvcTiered522Permissions.Authors.Default, L("Permission:Authors"));
+        authorsPermission.AddChild(AppMvcTiered522Permissions.Authors.Create, L("Permission:Authors.Create"));
+        authorsPermission.AddChild(AppMvcTiered522Permissions.Authors.Edit, L("Permission:Authors.Edit"));
+        authorsPermission.AddChild(AppMvcTiered522Permissions.Authors.Delete, L("Permission:Authors.Delete"));
     }
 
     private static LocalizableString L(string name)
